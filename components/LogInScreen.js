bbc24f492e99d19f7efbefef25a77f3868c67e7f
william ali <william.ali@hotmail.se>
2024-02-20 18:57:41 +0100

    Added logginscreen (pre build)

--- components/LogInScreen.js
+++ components/LogInScreen.js
@@ -28,13 +28,11 @@ const LogInScreen = () => {
 
   return (
     <>
-      <View>
+      <View style={styles.container}>
         <Image
           source={require("../assets/IsoleringsLandslaget.png")}
           style={styles.logInImage}
         />
-      </View>
-      <View style={styles.container}>
         <TextInput
           style={styles.input}
           placeholder="Email"
@@ -55,26 +53,28 @@ const LogInScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  logInImage: {
-    alignSelf: "center",
-    resizeMode: "contain",
-    width: 200,
-    height: 200,
-    marginVertical:"50%"
-  },
   container: {
     justifyContent: "center",
     alignItems: "center",
+    alignContent: "center",
     padding: 16,
+    marginTop:50,
+  },
+  logInImage: {
+    marginBottom:60,
+    resizeMode: "contain",
+    width: 200,
+    height: 200,
   },
   input: {
     width: "100%",
-    height: 50,
+    height: 60,
     borderColor: "gray",
     borderRadius: 8,
     borderWidth: 1,
-    marginBottom: 12,
     paddingHorizontal: 8,
+    fontSize: 20,
+    marginBottom:5,
   },
 });
 
